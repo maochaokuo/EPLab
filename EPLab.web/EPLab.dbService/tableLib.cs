2ef4ab20ab0e6976eba3b09f49e7db42bed76751
Thomas Mao <tm1813@gmail.com>
2020-07-28 09:36:02 +0800

    db service files

---
+++ EPLab.web/EPLab.dbService/tableLib.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPLab.dbService
+{
+    public class tableLib
+    {
+        // todo !!... 不想用ef, 出問題很難查, 就用dapper吧
+    }
+}
