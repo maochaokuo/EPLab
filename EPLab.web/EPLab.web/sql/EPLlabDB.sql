e7361acc4f1db56392ec9d0b44400ebb4e8bb4e8
Thomas Mao <tm1813@gmail.com>
2020-08-07 07:37:51 +0800

    backup

--- EPLab.web/EPLab.web/sql/EPLlabDB.sql
+++ EPLab.web/EPLab.web/sql/EPLlabDB.sql
@@ -1,5 +1,58 @@
 use EPLlabDB
 
+--declare @fieldlst varchar(999)=''
+
+--SELECT 
+--    @fieldlst += QUOTENAME(fieldName) + ','
+--FROM 
+--    fields
+--ORDER BY 
+--    defaultOrder;
+--SET @fieldlst = LEFT(@fieldlst, LEN(@fieldlst) - 1);
+
+--PRINT @fieldlst;
+--select @fieldlst
+
+--select fieldName
+--from fields f
+--order by f.defaultOrder
+/*
+DECLARE 
+    @columns NVARCHAR(MAX) = '', 
+    @sql     NVARCHAR(MAX) = '';
+
+-- select the category names
+SELECT 
+    @columns+=QUOTENAME(category_name) + ','
+FROM 
+    production.categories
+ORDER BY 
+    category_name;
+
+-- remove the last comma
+SET @columns = LEFT(@columns, LEN(@columns) - 1);
+
+-- construct dynamic SQL
+SET @sql ='
+SELECT * FROM   
+(
+    SELECT 
+        category_name, 
+        model_year,
+        product_id 
+    FROM 
+        production.products p
+        INNER JOIN production.categories c 
+            ON c.category_id = p.category_id
+) t 
+PIVOT(
+    COUNT(product_id) 
+    FOR category_name IN ('+ @columns +')
+) AS pivot_table;';
+
+-- execute the dynamic SQL
+EXECUTE sp_executesql @sql;
+*/
 select f.fieldName, fv.fieldValue
 from fieldValues fv
 join
@@ -7,7 +60,7 @@ join
 select r.rowId, fv1.fieldValue--, fv2.fieldValue
 from [rows] r
 join fieldValues fv1 on r.rowId=fv1.rowId 
-join fields f1 on fv1.fieldId=f1.fieldId and f1.fieldName='dealdate'
+join fields f1 on fv1.fieldId=f1.fieldId and f1.fieldName='dealdatetime'
 --join fieldValues fv2 on r.rowId=fv2.rowId 
 --join fields f2 on fv2.fieldId=f2.fieldId and f2.fieldName='dealtime'
 --where r.rowId='CA8F15B9-4893-4301-A05A-0010FAB6BF26'
@@ -17,6 +70,16 @@ join fields f on fv.fieldId=f.fieldId
 order by b.fieldValue,f.defaultOrder
 
 /*
+--select dealdate+dealtime dealdatetime, dealdate, dealtime, [open], high, low, [close], volume, dealmonth, section
+--from indices2.dbo.ohlc
+--where dealdate between '20180702' /*and '20180706'*/ and section=1
+--order by dealdate, dealtime
+
+ cross tabl ref: 
+http://www.sqlfiddle.com/#!18/a592d/7
+https://www.essentialsql.com/create-cross-tab-query-summarize-data-sql-server/
+https://www.mssqltips.com/sqlservertip/1019/crosstab-queries-using-pivot-in-sql-server/
+
 select 
 from [tables] t
 join fields f on t.tableId=f.fieldId
