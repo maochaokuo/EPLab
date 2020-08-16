c7772f22bf91d9f4b4684c2674f0688ba933e93e
Thomas Mao <tm1813@gmail.com>
2020-08-17 07:38:12 +0800

    big query result

---
+++ EPLab.web/EPlab.model.fwk/bigQueryResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPlab.model.fwk
+{
+    public class bigQueryResult
+    {
+        public List<string> columnNames { get; set; }
+        public List<object> columnValues { get; set; }
+
+        public string fromDataTable(DataTable dt)
+        {
+            string ret = "";
+            //todo !!...(2)bigQueryResult fromDataTable
+            return ret;
+        }
+        public DataTable toDataTable()
+        {
+            DataTable ret = null;
+            //todo !!...(2)bigQueryResult toDataTable
+            return ret;
+        }
+
+        public bigQueryResult()
+        {
+            columnNames = new List<string>();
+            columnValues = new List<object>();
+        }
+    }
+}
--- EPLab.web/EPlab.model.fwk/queryViewViewModel.cs
+++ EPLab.web/EPlab.model.fwk/queryViewViewModel.cs
@@ -1,4 +1,5 @@
 ﻿
+using EPlab.entity.fwk;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,35 @@ namespace EPlab.model.fwk
     [Serializable]
     public class queryViewViewModel : ViewModelBase
     {
+        public List<queryDisp> query2select { get; set; }
+        public queryDisp currentQuery { get; set; }
+        public List<string> queryPara ()
+        {
+            List<string> ret = null;
+            if (currentQuery == null)
+                return ret;
+            ret = new List<string>();
+            Guid? expId = currentQuery.whereExpressionId;
+            if (expId == null)
+                return ret;
+            expressions expr = null;//todo !!... (1) get by expId
+            if (!string.IsNullOrWhiteSpace(expr.para1externalName))
+                ret.Add(expr.para1externalName.Trim());
+            if (!string.IsNullOrWhiteSpace(expr.para2externalName))
+                ret.Add(expr.para2externalName.Trim());
+            if (!string.IsNullOrWhiteSpace(expr.para3externalName))
+                ret.Add(expr.para3externalName.Trim());
+            if (!string.IsNullOrWhiteSpace(expr.para4externalName))
+                ret.Add(expr.para4externalName.Trim());
+            if (!string.IsNullOrWhiteSpace(expr.para5externalName))
+                ret.Add(expr.para5externalName.Trim());
+            return ret;
+        }
+
+        public queryViewViewModel()
+        {
+            query2select = new List<queryDisp>();
+            currentQuery = new queryDisp();
+        }
     }
 }
