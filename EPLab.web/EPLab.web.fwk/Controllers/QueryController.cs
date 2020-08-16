f2fae8ea0919306810ccb9309c97727acd979e10
Thomas Mao <tm1813@gmail.com>
2020-08-16 23:06:28 +0800

    query controller n to do

---
+++ EPLab.web/EPLab.web.fwk/Controllers/QueryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPLab.web.fwk.Controllers
+{
+    public class QueryController : Controller
+    {
+        // todo !!... (1) 接下來query list, then pick 1 query, 輸入查詢欄位, 展示查詢值, 算是execute功能
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
